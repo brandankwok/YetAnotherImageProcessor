d653d6de8afee08e476bd8e6c56ec6573547c2a4
brandankwok <brandankwok@gmail.com>
2021-11-24 16:33:22 -0800

    added heading stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # CMPT 120 Yet Another Image Processer
 # Starter code for main.py
-# Author(s):
-# Date:
+# Author(s): Brandan Kwok, Kyle Deliyannides
+# Date: December 6th, 2021
 # Description:
 
 import cmpt120imageProjHelper
