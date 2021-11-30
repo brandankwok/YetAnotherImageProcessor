3229b4641b3c1d1dfbeec9e1193e8aeb4cefc253
Kyle <kyledeliyannides@gmail.com>
2021-11-29 18:16:53 -0800

    update titles

--- main.py
+++ main.py
@@ -114,54 +114,54 @@ def handleUserInput(state, img):
                 # ***instead of setting the value of state["mode"]***
                 # ***it is ok to go a bit beyond 100 characters when calling the showUserInterface***
                 img = cmpt120imageManip.applyRed(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Red Filter ", generateMenu(state))
 
             if userInput == "2":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 img = cmpt120imageManip.applyGreen(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Green Filter ", generateMenu(state))
 
             if userInput == "3":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 img = cmpt120imageManip.applyBlue(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Blue Filter ", generateMenu(state))
 
             if userInput == "4":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 img = cmpt120imageManip.applySepia(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Sepia Filter ", generateMenu(state))
 
             if userInput == "5":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 img = cmpt120imageManip.applyWarm(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Warm Filter ", generateMenu(state))
 
             if userInput == "6":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 img = cmpt120imageManip.applyCold(img)
-                cmpt120imageProjHelper.showInterface(img, basic[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Apply Cold Filter ", generateMenu(state))
 
             if userInput == "7":
                 print("Log: Performing " + basic[int(userInput) - 1])
                 state["mode"] = "advanced"
-                cmpt120imageProjHelper.showInterface(img, "Switch to Advanced Options", generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Advanced Options ", generateMenu(state))
         #Advanced options...
         elif state["mode"] == "advanced":
 
             if userInput == "1":
                 print("Log: Performing " + advanced[int(userInput) - 1])
                 img = cmpt120imageManip.rotateLeft(img)
-                cmpt120imageProjHelper.showInterface(img, advanced[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Rotate Left ", generateMenu(state))
 
             if userInput == "2":
                 print("Log: Performing " + advanced[int(userInput) - 1])
                 img = cmpt120imageManip.rotateRight(img)
-                cmpt120imageProjHelper.showInterface(img, advanced[int(userInput) - 1], generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Rotate Right ", generateMenu(state))
 
             if userInput == "6":
                 print("Log: Performing " + advanced[int(userInput) - 1])
                 state["mode"] = "basic"
-                cmpt120imageProjHelper.showInterface(img, "Switch to Basic Options", generateMenu(state))
+                cmpt120imageProjHelper.showInterface(img, "Basic Options ", generateMenu(state))
 
     else: # unrecognized user input
         print("Log: Unrecognized user input: " + userInput)
