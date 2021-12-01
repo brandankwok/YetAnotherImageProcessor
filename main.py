abe80ab1a75284f8e0a2ebd222ccfdccd29f39d0
Kyle <kyledeliyannides@gmail.com>
2021-11-30 18:45:13 -0800

    fish has been located

--- main.py
+++ main.py
@@ -163,6 +163,11 @@ def handleUserInput(state, img):
                 img = cmpt120imageManip.sizeDouble(img)
                 cmpt120imageProjHelper.showInterface(img, "Double Size ", generateMenu(state))
 
+            if userInput == "5":
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                img = cmpt120imageManip.locateFish(img)
+                cmpt120imageProjHelper.showInterface(img, "Locate Fish ", generateMenu(state))
+
             if userInput == "6":
                 print("Log: Performing " + advanced[int(userInput) - 1])
                 state["mode"] = "basic"
